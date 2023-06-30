a00333d1b151c512f1677fb12780d9b3f11483c1
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-06-30 13:56:28 +0600

    Connection MongoDB and get products

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://<username>:<password>@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://portfolioUser:uSnbSuDkDjtnHyQH@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +28,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const projectCollection = client.db("portfolioDB").collection("projects");
+
+    app.get("/projects", async (req, res) => {
+      const result = await projectCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
