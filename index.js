fecbb301141b72987ffccfede86b227f0aac3d5a
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-07-01 12:01:13 +0600

    Added Database Secure

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(express.json());
 //uSnbSuDkDjtnHyQH
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://portfolioUser:uSnbSuDkDjtnHyQH@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0eicqxw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
